83dd5790fdb4fc5a04a22bfe1304ab72a7de5770
Anthony Rey <anthony.rey5@gmail.com>
2023-07-13 13:57:04 +0200

    initial commit

---
+++ UWPWindowsWrapper/UWPWindowsWrapper.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include <gamingdeviceinformation.h>
+#include "UWPWindowsWrapper.h"
+
+//Thank you microsoft, I searched what to link for 2 HOURS
+//https://learn.microsoft.com/fr-fr/uwp/win32-and-com/win32-apis
+
+GAMING_DEVICE_DEVICE_ID GetGamingDeviceModelInfos();
+GAMING_DEVICE_DEVICE_ID GetGamingDeviceModelInfos()
+{
+    GAMING_DEVICE_MODEL_INFORMATION infos;
+    if (FAILED(GetGamingDeviceModelInformation(&infos)))
+    {
+        return GAMING_DEVICE_DEVICE_ID_NONE;
+    }
+    return infos.deviceId;
+}
+
+bool IsXbox()
+{
+    return GetGamingDeviceModelInfos() != GAMING_DEVICE_DEVICE_ID_NONE;
+}
+
+bool IsXboxOne()
+{
+    return GetGamingDeviceModelInfos() == GAMING_DEVICE_DEVICE_ID_XBOX_ONE;
+}
+
+bool IsXboxOneS()
+{
+    return GetGamingDeviceModelInfos() == GAMING_DEVICE_DEVICE_ID_XBOX_ONE_S;
+}
+
+bool IsXboxOneX()
+{
+    return GetGamingDeviceModelInfos() == GAMING_DEVICE_DEVICE_ID_XBOX_ONE_X;
+}
+
+bool IsXboxSeriesS()
+{
+    return GetGamingDeviceModelInfos() == GAMING_DEVICE_DEVICE_ID_XBOX_SERIES_S;
+}
+
+bool IsXboxSeriesX()
+{
+    return GetGamingDeviceModelInfos() == GAMING_DEVICE_DEVICE_ID_XBOX_SERIES_X;
+}
---
+++ UWPWindowsWrapper/UWPWindowsWrapper.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#define APIWRAPPER_API __declspec(dllexport)
+
+extern "C" {
+
+	bool APIWRAPPER_API IsXbox();
+	bool APIWRAPPER_API IsXboxOne();
+	bool APIWRAPPER_API IsXboxOneS();
+	bool APIWRAPPER_API IsXboxOneX();
+
+	//Needs 10.0.22621.0
+	//We still want people with Windows 10 to play this game
+	//TODO Handle user calling this when OS is too old : 
+	//https://learn.microsoft.com/en-gb/windows/uwp/updates-and-versions/choose-a-uwp-version?ocid=VSClient_VerX_NewProject_version
+	bool APIWRAPPER_API IsXboxSeriesS();
+	bool APIWRAPPER_API IsXboxSeriesX();
+}
